7e567bc9209fa29c88f37da8975ac120d8b79e85
Leandro Scardua <leandroscar@gmail.com>
2022-06-09 11:35:38 -0400

    Update install.ps1

--- packer/win_rancher/install.ps1
+++ packer/win_rancher/install.ps1
@@ -1,2 +1,5 @@
 # Get the information from teh Windows Node Registration
 curl.exe --insecure -fL {{ enter the  rancher IP here like https://xxx.xxx.xxx.xxx }}/wins-agent-install.ps1 -o install.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; ./install.ps1 -Server {{ enter the  rancher IP here like https://xxx.xxx.xxx.xxx }} -Label 'cattle.io/os=windows' -Token {{ enter Token here }} -Worker -CaChecksum {{ enter CaChecksum }}
+
+# Open Firewall port to enable OOB CPU and Memory metrics
+New-NetFirewallRule -DisplayName "Allow Metric OOB" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 10248-10256
